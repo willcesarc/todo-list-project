2a81f0a8a077c1aeeb6bf5ca4dbc72e9dfe5508b
willcesarc <wilheracccc@gmail.com>
2025-03-24 18:34:40 -0300

    config: update Tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [],
