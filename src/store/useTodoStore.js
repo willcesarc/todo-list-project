7471f56852d47c9477a195de6d88ff71a0acdfe5
willcesarc <wilheracccc@gmail.com>
2025-03-24 19:26:08 -0300

    feat: integrate Zustand localStorage

--- src/store/useTodoStore.js
+++ src/store/useTodoStore.js
@@ -1,21 +1,27 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useTodoStore = create((set) => ({
-  todos: [],
-  addTodo: (text) => 
-    set((state) => ({
-      todos: [...state.todos, { id: Date.now(), text, completed: false }]
-    })),
-  toggleTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    })),
-  deleteTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.filter(todo => todo.id !== id)
-    }))
-}));
+const useTodoStore = create(
+  persist(
+    (set) => ({
+      todos: [],
+      addTodo: (text) => set((state) => ({
+        todos: [...state.todos, { id: Date.now(), text, completed: false }]
+      })),
+      toggleTodo: (id) => set((state) => ({
+        todos: state.todos.map(todo =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        )
+      })),
+      deleteTodo: (id) => set((state) => ({
+        todos: state.todos.filter(todo => todo.id !== id)
+      })),
+      clearTodos: () => set({ todos: [] }, true)
+    }),
+    {
+      name: 'todo-storage',
+    }
+  )
+);
 
 export default useTodoStore;
